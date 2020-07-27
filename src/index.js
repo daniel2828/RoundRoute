f869aac747b04565a396d6581ce6c669578278c0
DESKTOP-581QSJQ\daniel2828 <danieltendero@hotmail.es>
2020-07-27 20:26:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './bootstrap.min.css';
-import './index.scss';
+import './index.css';
 import Examples from './examples';
 
 ReactDOM.render(<Examples />, document.getElementById('root'));
